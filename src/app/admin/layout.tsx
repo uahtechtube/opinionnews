6f87aea9e7078ea06e3636abd41a469a39b99bed
Umar Alhaji Hussaini <umaralhajihussaini@gmail.com>
2025-09-10 12:20:10 +0000

    remover the navbar on the admin pannel

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,6 +1,5 @@
 import AdminSidebar from '@/components/layout/admin-sidebar';
 import { SidebarProvider } from '@/components/ui/sidebar';
-import Header from '@/components/layout/header';
 
 export default function AdminLayout({
   children,
@@ -9,14 +8,11 @@ export default function AdminLayout({
 }) {
   return (
     <SidebarProvider>
-      <div className="flex min-h-screen flex-col">
-        <Header />
-        <div className="flex flex-1">
-          <AdminSidebar />
-          <main className="flex-1 bg-muted/40 p-4 md:p-6 lg:p-8">
-            {children}
-          </main>
-        </div>
+      <div className="flex min-h-screen">
+        <AdminSidebar />
+        <main className="flex-1 bg-muted/40 p-4 md:p-6 lg:p-8">
+          {children}
+        </main>
       </div>
     </SidebarProvider>
   );
